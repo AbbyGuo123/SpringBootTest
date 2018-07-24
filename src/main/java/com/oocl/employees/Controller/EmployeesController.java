ea78637c1c37213e46b277910cc610425dc37ed5
GUOAB <AbbyGuo@oocl.com>
2018-07-24 17:12:49 +0800

    add deleteEmployee and modifyEmployee func

--- src/main/java/com/oocl/employees/Controller/EmployeesController.java
+++ src/main/java/com/oocl/employees/Controller/EmployeesController.java
@@ -15,41 +15,42 @@ public class EmployeesController {
 //    @Autowired
 //    private EmployeeService employeeService;
 
-    @GetMapping("/getAllEmployees")
+    List<Employee> employees = new ArrayList<>();
+    public EmployeesController() {
+        employees.add(new Employee(1,"name1",1,"man"));
+        employees.add(new Employee(2,"name2",2,"man"));
+    }
+
+    @GetMapping("")
     public List<Employee> getAllEmployees(){
-        List<Employee> employees = initEmployee();
         return employees;
 
     }
 
-    @GetMapping("/getEmployeesById")
-    public Employee getEmployeesById(int id){
-        List<Employee> employees = initEmployee();
+    @GetMapping("/{id}")
+    public Employee getEmployeesById(@PathVariable int id){
         Employee employee = employees.stream().filter(e->e.getId()==id).collect(Collectors.toList()).get(0);
         return employee;
     }
 
-    @PostMapping("/addEmployee")
+    @PostMapping("")
     public List<Employee> addEmployee(@RequestBody Employee employee){
-        List<Employee> employees = initEmployee();
+        employees.add(employee);
+        return employees;
+    }
+    @DeleteMapping("/{id}")
+    public List<Employee> deleteEmployee(@PathVariable int id){
+        Employee employee = employees.stream().filter(e->e.getId()==id).collect(Collectors.toList()).get(0);
+        employees.remove(employee);
+        return employees;
+    }
+
+    @PatchMapping("")
+    public List<Employee> modifyEmployeeById(@RequestBody Employee employee){
+        Employee oldEmployee = employees.stream().filter(e->e.getId()==employee.getId()).collect(Collectors.toList()).get(0);
+        employees.remove(oldEmployee);
         employees.add(employee);
         return employees;
     }
 
-//    @PatchMapping("/modifyEmployeeById")
-//    public List<Employee> modifyEmployeeById(int id,int modifyAge){
-//        List<Employee> employees = initEmployee();
-//        Employee employee = employees.stream().filter(e->e.getId()==id).collect(Collectors.toList()).get(0);
-//        employees.remove(employee);
-//        employee.setAge(modifyAge);
-//        employees.add(employee);
-//        return employees;
-//    }
-
-    public List<Employee> initEmployee(){
-        Employee employee = new Employee(1,"name",1,"man");
-        List<Employee> list = new ArrayList<>();
-        list.add(employee);
-        return list;
-    }
 }

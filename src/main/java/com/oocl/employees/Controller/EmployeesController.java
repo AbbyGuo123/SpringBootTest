6c18a9529d5425b73682365b9ee288455d7d28ba
GUOAB <AbbyGuo@oocl.com>
2018-07-24 14:57:34 +0800

    [GUOAB] addEmployee

--- src/main/java/com/oocl/employees/Controller/EmployeesController.java
+++ src/main/java/com/oocl/employees/Controller/EmployeesController.java
@@ -23,13 +23,28 @@ public class EmployeesController {
     }
 
     @GetMapping("/getEmployeesById")
-    public Employee getEmployeesById(@RequestBody int id){
+    public Employee getEmployeesById(int id){
         List<Employee> employees = initEmployee();
         Employee employee = employees.stream().filter(e->e.getId()==id).collect(Collectors.toList()).get(0);
         return employee;
-
     }
 
+    @PostMapping("/addEmployee")
+    public List<Employee> addEmployee(@RequestBody Employee employee){
+        List<Employee> employees = initEmployee();
+        employees.add(employee);
+        return employees;
+    }
+
+//    @PatchMapping("/modifyEmployeeById")
+//    public List<Employee> modifyEmployeeById(int id,int modifyAge){
+//        List<Employee> employees = initEmployee();
+//        Employee employee = employees.stream().filter(e->e.getId()==id).collect(Collectors.toList()).get(0);
+//        employees.remove(employee);
+//        employee.setAge(modifyAge);
+//        employees.add(employee);
+//        return employees;
+//    }
 
     public List<Employee> initEmployee(){
         Employee employee = new Employee(1,"name",1,"man");
